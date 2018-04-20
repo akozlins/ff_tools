28699fd23d18595ecc0fa893f28832c691fc518c
Alexandr Kozlinskiy <akozlins@gmail.com>
2018-04-20 15:36:36 +0200

    update Makefile, version 1.0.1

--- Makefile
+++ Makefile
@@ -2,4 +2,9 @@
 .PHONY: tab-retach
 tab-retach:
 	convert -strip -size 48x48 canvas:black $@/icon.png
-	zip -r -FS $@.zip $@/*
+	( cd $@ ; zip -r -FS ../$@.zip * )
+
+.PHONY: tab-reload
+tab-reload:
+	convert -strip -size 48x48 canvas:black $@/icon.png
+	( cd $@ ; zip -r -FS ../$@.zip * )
--- tab-retach/main.js
+++ tab-retach/main.js
@@ -3,7 +3,7 @@
 let wid;
 async function getCurrentWindow() {
     let w = await browser.windows.getCurrent({});
-    console.log("retach:", "currentWindow =", w);
+    console.debug("retach:", "currentWindow =", w);
     wid = w.id;
 }
 getCurrentWindow();
@@ -20,12 +20,12 @@ async function onDetach(tabId, info) {
     };
 
     let tab = await browser.tabs.get(tabId);
-    console.log("retach:", "wid =", wid, "tab.windowId =", tab.windowId);
-    console.log("retach/before:", "tab =", tab);
+    console.debug("retach:", "wid =", wid, "tab.windowId =", tab.windowId);
+    console.debug("retach/before:", "tab =", tab);
     let tabs = await browser.tabs.move(tabId, props);
     for(let i = 0; i < tabs.length; i++) {
         let tab = await browser.tabs.update(tabs[i].id, { active : true });
-        console.log("retach/after:", "tab =", tab);
+        console.debug("retach/after:", "tab =", tab);
     }
 
     busy = false;
